8d0d7ea616be830a87f27fdf52d5e39023221c5b
Brock Chelle <Brocklchelle@gmail.com>
2021-01-27 08:29:24 -0700

    Fixed ?? to ||

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
-const PORT = process.env.PORT ?? 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
